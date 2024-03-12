8a42711fa4dcb880ea20354fea285dc7e538fe7c
Tkachenko_Yuriy <uvt86@ukr.net>
2024-03-12 12:00:18 +0200

    update 2/3/6

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -10,10 +10,10 @@ type AllType = {
   weight: number;
 };
 
-function compare<T extends AllType, U extends AllType>(
-  top: Pick<T, keyof AllType>,
-  bottom: Pick<U, keyof AllType>
-): AllType {
+function compare<
+  T extends Pick<AllType, "name" | "color">,
+  U extends Pick<AllType, "position" | "weight">
+>(top: T, bottom: U): AllType {
   return {
     name: top.name,
     color: top.color,
@@ -21,3 +21,4 @@ function compare<T extends AllType, U extends AllType>(
     weight: bottom.weight,
   };
 }
+
